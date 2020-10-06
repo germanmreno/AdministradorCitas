e97a715dc4a4afb742969392eea63c0a5c8a3ec4
Germán Moreno <grmmrno@gmail.com>
2020-10-06 12:29:12 -0400

    Agregando Service Workers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-import App from "./Clases/App.js";
-
-const app = new App();
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/sw.js")
+        .then((registrado) => console.log("Se instaló correctamente", registrado))
+        .catch((error) => {
+            console.log("Falló la instalación", error);
+        });
+} else {
+    console.log("Service Workers no soportados");
+}
---
+++ sw.js
@@ -0,0 +1,48 @@
+const nombreCache = "apv-v5";
+const archivos = [
+    "/",
+    "/index.html",
+    "/error.html",
+    "/css/bootstrap.css",
+    "/css/styles.css",
+    "/js/app.js",
+    "/js/apv.js",
+];
+
+self.addEventListener("install", (e) => {
+    console.log("Instalado el Service Worker");
+
+    e.waitUntil(
+        caches.open(nombreCache).then((cache) => {
+            console.log("Cacheando");
+            cache.addAll(archivos);
+        })
+    );
+});
+
+self.addEventListener("activate", (e) => {
+    console.log("Service Worker activado");
+
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                    .filter((key) => key !== nombreCache)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    console.log("Fetch...", e);
+
+    e.respondWith(
+        caches
+            .match(e.request)
+            .then((respuestaCache) => {
+                return respuestaCache;
+            })
+            .catch(() => caches.match("/error.html"))
+    );
+});
